b34fd4221f8e21e998aaa50ccd22f0abf20cfa98
Gary Feng <gary.feng@gmail.com>
2019-05-18 21:04:55 -0400

    Adding project files

---
+++ helloworld.js
@@ -0,0 +1,8 @@
+exports.handler = function(event, context, callback) {
+  var response = {
+      isBase64Encoded: false,
+      statusCode: 200,
+      body: 'Gary says hellow wrold\n\n'
+  };
+  callback(null, response);
+};
